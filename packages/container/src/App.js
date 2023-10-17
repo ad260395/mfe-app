b37158f5cdaf379a289eea8dd35b7372df4b1681
Ashish D <ashish.deshpande@amerisourcebergen.com>
2023-10-17 16:21:48 +0530

    "Initial commit"

---
+++ packages/container/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+import MarketingApp from "./components/MarketingApp";
+import AuthApp from "./components/AuthApp";
+import Header from "./components/Header";
+
+/* To avoid CSS name collision */
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
+export default () => {
+  return (
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/auth" component={AuthApp} />
+            <Route path="/" component={MarketingApp} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
+  );
+};
